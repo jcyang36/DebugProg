13883370e5b56a8336b0bdbd4824508cef1a9400
Joyce Yang <jcyang@hmc.edu>
2017-10-11 13:40:00 -0400

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Double x,y;
+
+	x=3.1415;
+	y=3.64;
+
+	System.out.println("pi is approximately " +x);
+	System.out.println("My GPA was "+y);
+    }
+}
